eadd2167e9237f0fb195c88b33c18b0169e59b8a
Michael Campbell <me@micycle.dev>
2025-09-10 14:57:30 -0400

    testing 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const github = require('@actions/github');
+const token = process.env.GITHUB_TOKEN;
+const octokit = github.getOctokit(token);
+
+const { owner, repo } = github.context.repo;
+const issue_number = github.context.payload.issue.number;
+
+const { data: linkedIssues } = await octokit.request(
+  "GET /repos/{owner}/{repo}/issues/{issue_number}/linked",
+  { owner, repo, issue_number }
+);
+
+for (const item of linkedIssues) {
+  console.log(`Issue #${item.number} - relationship: ${item.relation_type}`);
+}
+
