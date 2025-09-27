b5398bf62584f5402bedeffdd624127d94bc35bb
Michael Campbell <me@micycle.dev>
2025-09-27 12:40:55 -0400

    fix assign bug

--- src/index.js
+++ src/index.js
@@ -21,9 +21,16 @@ try {
     // find our "target", the person who just got assigned
     const target_login = github.context.payload.assignee.login;
 
+    // find the other assignees to the parent issue
+    
+    const { assignees } = await octokit.request(
+        "GET /repos/{owner}/{repo}/issues/{issue_number}",
+        { owner, repo, issue_number: parentIssue.number }
+    );
+
     await octokit.request(
         "POST /repos/{owner}/{repo}/issues/{issue_number}/assignees",
-        { owner, repo, issue_number: parentIssue.number, assignees: [ target_login ] }
+        { owner, repo, issue_number: parentIssue.number, assignees: [ ...assignees, target_login ] }
     );
 } catch (error) {
     if (error.status === 404) {
